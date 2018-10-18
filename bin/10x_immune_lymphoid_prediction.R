9287efeeab4b6bcd70b5a92ce23fe3b5bf1adb74
joseah <alquicirajose@gmail.com>
2018-10-18 17:16:51 +1100

    comments lines to process training data only

--- bin/10x_immune_lymphoid_prediction.R
+++ bin/10x_immune_lymphoid_prediction.R
@@ -38,8 +38,8 @@ input <- file.path("results", "2018-10-12_10x_immune_myeloid_vs_lymphoid_predict
 
 # Read files
 
-filename <- "predictions.RDS" # <------ Input file
-predictions <- readRDS(here(input, filename))
+# filename <- "predictions.RDS" # <------ Input file
+# predictions <- readRDS(here(input, filename))
 
 
 input <- file.path("results", "2018-10-12_10x_immune_tree") # <------ Input directory
@@ -49,33 +49,34 @@ filename <- "train_data.RDS" # <------ Input file
 train_data <- readRDS(here(input, filename))
 
 
-filename <- "pred_data.RDS" # <------ Input file
-pred_data <- readRDS(here(input, filename))
+# filename <- "pred_data.RDS" # <------ Input file
+# pred_data <- readRDS(here(input, filename))
 
 # Subset cells
 
-pred_data@meta.data$pred1 <- as.factor(predictions$predClass)
+# pred_data@meta.data$pred1 <- as.factor(predictions$predClass)
 
 # train_data <- RunTSNE(train_data)
 # TSNEPlot(train_data, group = "cellType1")
 
 
 i <- rownames(train_data@meta.data)[train_data@meta.data$cellType2 %in% c("Lymphoid_cell")]
-j <- rownames(pred_data@meta.data)[pred_data@meta.data$pred1 %in% c("Lymphoid_cell")]
+# j <- rownames(pred_data@meta.data)[pred_data@meta.data$pred1 %in% c("Lymphoid_cell")]
 
 
 train_data <- SubsetData(train_data, cells.use = i, do.clean = TRUE)
 train_data@meta.data$cellType2 <- factor(train_data@meta.data$cellType2)
 
-pred_data <- SubsetData(pred_data, cells.use = j, do.clean = TRUE)
-pred_data@meta.data$pred1 <- factor(pred_data@meta.data$pred1)
+# pred_data <- SubsetData(pred_data, cells.use = j, do.clean = TRUE)
+# pred_data@meta.data$pred1 <- factor(pred_data@meta.data$pred1)
 
 
 train_data@meta.data$cellType1 <- factor(train_data@meta.data$cellType1)
 
-gc()
 
 # Process data ------------------------------------------------------------
+
+cat("\nProcessing training data (normalization, scaling, and PCA)...\n")
 
 train_data <- train_data %>% 
   NormalizeData(normalization.method = "LogNormalize", 
@@ -91,28 +92,31 @@ train_data <- train_data %>%
          pcs.print = 1:5, 
          genes.print = 5)
 
-PCAPlot(train_data, group = "cellType1") -> q
 
 
 # Get feature space -------------------------------------------------------
 
+cat("\nGetting feature space...\n")
 sc_pred <- getFeatureSpace(train_data, pVar = "cellType1")
 rm(train_data)
+
+cat("\nSaving scPred object...\n")
+saveRDS(sc_pred, file = here(output, "scpred.RDS"))
 
 
 # Train models ------------------------------------------------------------
 
-library(doParallel)
-cl <- makePSOCKcluster(5)
-registerDoParallel(cl)
-sc_pred <- trainModel(sc_pred)
-stopCluster(cl)
-
-predictions$true <- pred_data@meta.data$cellType2
-
-
-saveRDS(predictions, file = here(output, "predictions.RDS"))
-saveRDS(sc_pred, file = here(output, "scpred.RDS"))
+# library(doParallel)
+# cl <- makePSOCKcluster(5)
+# registerDoParallel(cl)
+# sc_pred <- trainModel(sc_pred)
+# stopCluster(cl)
+# 
+# predictions$true <- pred_data@meta.data$cellType2
+# 
+# 
+# saveRDS(predictions, file = here(output, "predictions.RDS"))
+# saveRDS(sc_pred, file = here(output, "scpred.RDS"))
 
 
 # scPred version: d7661592
